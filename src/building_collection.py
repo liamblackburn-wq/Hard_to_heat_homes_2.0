529ef729c06c79371e309e2c6e82e68d4273874d
Etido Ukpong <etido.ukpong@madetech.com>
2025-12-17 15:25:41 +0000

    pass test checking that correct keys are in list items

--- src/building_collection.py
+++ src/building_collection.py
@@ -3,5 +3,5 @@ class BuildingCollection:
         self.pages = pages
     def produce_list(self):
         if self.pages != 0:
-            return [{}] * (self.pages * 100)
+            return [{"id":0, "type":0, "geometry": 0, "properties": 0}] * (self.pages * 100)
         return []

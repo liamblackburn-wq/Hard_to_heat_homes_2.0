bf1c1b4c5ab15536534badb0ec7dec0f94de53af
Joe Rawnsley <joe.rawnsley@madetech.com>
2025-12-17 14:08:27 +0000

    create failing test for desired list length of 2 pages

--- tests/test_building_collection.py
+++ tests/test_building_collection.py
@@ -9,7 +9,11 @@ bristol_bbox = {
 
 bristol_bbox_string = str(bristol_bbox["minx"]) + str(bristol_bbox["miny"]) + str(bristol_bbox["maxx"]) + str(bristol_bbox["maxy"])
 
-def test_produce_list_returns_an_empty_list_if_desired_list_length_is_zero():
+def test_produce_list_returns_an_empty_list_if_pages_is_0():
     assert BuildingCollection(bristol_bbox_string, 0).produce_list() == []
-def test_produce_list_returns_list_of_100_properties_if_desired_list_length_is_100():
-    assert len(BuildingCollection(bristol_bbox_string, 100).produce_list()) == 100
+    
+def test_produce_list_returns_list_of_100_properties_if_pages_is_1():
+    assert len(BuildingCollection(bristol_bbox_string, 1).produce_list()) == 100
+    
+def test_produce_list_returns_list_of_200_properties_if_pages_is_2():
+    assert len(BuildingCollection(bristol_bbox_string, 2).produce_list()) == 200

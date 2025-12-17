5595e3c9a2edc611f047becb658278a7bdd35f12
Joe Rawnsley <joe.rawnsley@madetech.com>
2025-12-17 12:27:05 +0000

    first test for newly created BuildingCollection class

---
+++ src/building_collection.py
@@ -0,0 +1,5 @@
+class BuildingCollection:
+    def __init__(self, council_bbox, list_length):
+        pass
+    def produce_list(self):
+        pass
---
+++ tests/test_building_collection.py
@@ -0,0 +1,13 @@
+from src.building_collection import BuildingCollection
+
+bristol_bbox = {
+      "minx": -3.117815916947311,
+      "miny": 51.34162559147916,
+      "maxx": -2.510419161285424,
+      "maxy": 51.54443273743301
+    }
+
+bristol_bbox_string = str(bristol_bbox["minx"]) + str(bristol_bbox["miny"]) + str(bristol_bbox["maxx"]) + str(bristol_bbox["maxy"])
+
+def test_produce_list_returns_an_empty_list_if_desired_list_length_is_zero():
+    assert BuildingCollection(bristol_bbox_string, 0).produce_list() == []

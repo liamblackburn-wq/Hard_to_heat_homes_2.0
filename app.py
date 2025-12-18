07c65481b3abbe4ee44e0419f5afe1fde90b1ea5
Wren Hawthorne <wren.hawthorne@madetech.com>
2025-12-18 14:18:04 +0000

    removed duplicate logic

--- app.py
+++ app.py
@@ -34,7 +34,6 @@ def set_property_data(council_code, council_bbox):
     properties = get_properties_from_os(list_of_buildings)
     properties = filter_properties_by_council_code(council_code, properties)
     properties = get_attributes_from_epc(properties)
-    properties = remove_blank_addresses(properties)
 
     for i in range(len(properties)):
         properties[i].calculate_score()
